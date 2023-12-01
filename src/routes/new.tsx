23651f3de114d5b30b9e4da6b5d2d4c9fc01f06a
Kyleigh Poultney <kyleigh.poultney@gmail.com>
2023-12-01 13:25:33 -0500

    Updated form inputs

--- src/routes/new.tsx
+++ src/routes/new.tsx
@@ -6,66 +6,66 @@ export const New = () => {
       <h2>
         New Meal Plan
       </h2>
-      <Form>
-        <label htmlFor="dietaryType">Dietary Type:</label><br />
+      <Form replace >
+        <label htmlFor="dietaryType"><h3>Do you follow a specific diet?</h3></label><br />
         <input type="radio" name="dietaryType" id="vegetarian" value="vegetarian" />
-        <label htmlFor="vegetarian">Vegetarian</label>
-        <input type="radio" name="dietaryType" id="redMeat" value="redMeat" />
-        <label htmlFor="redMeat">No Red Meat</label>
-        <input type="radio" name="dietaryType" id="vegan" value="vegan" />
-        <label htmlFor="vegan">Vegan</label>
-        <input type="radio" name="dietaryType" id="dairyFree" value="dairy-free" />
-        <label htmlFor="dairyFree">Dairy Free</label>
+        <label htmlFor="vegetarian">Vegetarian</label><br />
         <input type="radio" name="dietaryType" id="paleo" value="paleo" />
-        <label htmlFor="paleo">Paleo</label>
+        <label htmlFor="paleo">Paleo</label><br />
         <input type="radio" name="dietaryType" id="pescatarian" value="pescatarian" />
-        <label htmlFor="pescatarian">Pescatarian</label>
-        <input type="radio" name="dietaryType" id="gluten" value="gluten" />
-        <label htmlFor="gluten">Gluten Free</label>
+        <label htmlFor="pescatarian">Pescatarian</label><br />
+        <input type="radio" name="dietaryType" id="keto" value="keto" />
+        <label htmlFor="keto">Keto</label><br />
+        <input type="radio" name="lowFat" id="lowFat" />
+        <label htmlFor="lowFat">Low Fat</label>< br />
+        <input type="radio" name="none" id="none" />
+        <label htmlFor="none">None</label>
         <br /><br />
 
-        <label htmlFor="allergies">Allergies/Dislikes:</label><br />
-        <input type="checkbox" name="" id="" value="" />
-        <label htmlFor=""></label>
-        <input type="checkbox" name="" id="" value="" />
-        <label htmlFor=""></label>
-        <input type="checkbox" name="" id="" value="" />
-        <label htmlFor=""></label>
-        <input type="checkbox" name="" id="" value="" />
-        <label htmlFor=""></label>
-        <input type="checkbox" name="" id="" value="" />
-        <label htmlFor=""></label>
-        <input type="checkbox" name="" id="" value="" />
+        <label htmlFor="allergies"><h3>Do you have any allergies or dislikes?</h3></label><br />
+        <input type="checkbox" name="eggs" id="eggs" value="eggs" />
+        <label htmlFor="eggs">Eggs</label><br />
+        <input type="checkbox" name="milk" id="milk" value="milk" />
+        <label htmlFor="milk">Milk</label><br />
+        <input type="checkbox" name="peanutes" id="peanuts" value="peanuts" />
+        <label htmlFor="peanuts">Peanuts</label><br />
+        <input type="checkbox" name="shellfish" id="shellfish" value="shellfish" />
+        <label htmlFor="shellfish">Shellfish</label><br />
+        <input type="checkbox" name="sesame" id="sesame" value="sesame" />
+        <label htmlFor="sesame">Sesame</label><br />
+        <input type="checkbox" name="soy" id="soy" value="soy" />
+        <label htmlFor="soy">Soy</label><br />
+        <input type="checkbox" name="treenuts" id="treenuts" value="treenuts" />
+        <label htmlFor="treenuts">Tree Nuts</label><br />
+        <input type="checkbox" name="wheat" id="wheat" />
+        <label htmlFor="wheat">Wheat</label>
         <br /><br />
 
-        <label htmlFor="daysOfWeek">Days of the Week:</label><br />
+        <label htmlFor="daysOfWeek"><h3>Which days do you need a meal plan for?</h3></label><br />
         <input type="checkbox" id="mon" name="daysOfWeek" value="mon" />
-        <label htmlFor="mon">Monday</label>
+        <label htmlFor="mon">Monday</label>  <input type="checkbox" id="fri" name="daysOfWeek" value="fri" />
+        <label htmlFor="fri">Friday</label><br />
         <input type="checkbox" id="tues" name="daysOfWeek" value="tues" />
-        <label htmlFor="tues">Tuesday</label>
+        <label htmlFor="tues">Tuesday</label> <input type="checkbox" id="sat" name="daysOfWeek" value="sat" />
+        <label htmlFor="sat">Saturday</label><br />
         <input type="checkbox" id="wed" name="daysOfWeek" value="wed" />
-        <label htmlFor="wed">Wednesday</label>
+        <label htmlFor="wed">Wednesday</label>  <input type="checkbox" id="sun" name="daysOfWeek" value="sun" />
+        <label htmlFor="sun">Sunday</label><br />
         <input type="checkbox" id="thur" name="daysOfWeek" value="thur" />
-        <label htmlFor="thur">Thursday</label>
-        <input type="checkbox" id="fri" name="daysOfWeek" value="fri" />
-        <label htmlFor="fri">Friday</label>
-        <input type="checkbox" id="sat" name="daysOfWeek" value="sat" />
-        <label htmlFor="sat">Saturday</label>
-        <input type="checkbox" id="sun" name="daysOfWeek" value="sun" />
-        <label htmlFor="sun">Sunday</label><br /><br />
+        <label htmlFor="thur">Thursday</label><br /><br />
 
-        <label htmlFor="mealsOfDay">Meals of the Day:</label><br />
+        <label htmlFor="mealsOfDay"><h3>Which meals do you need?</h3></label><br />
         <input type="checkbox" id="breakfast" name="mealsOfDay" value="breakfast" />
-        <label htmlFor="breakfast">Breakfast</label>
+        <label htmlFor="breakfast">Breakfast</label>&emsp; &emsp; &emsp;
         <input type="checkbox" id="lunch" name="mealsOfDay" value="lunch" />
-        <label htmlFor="lunch">Lunch</label>
+        <label htmlFor="lunch">Lunch</label>&emsp; &emsp; &emsp;
         <input type="checkbox" id="dinner" name="mealsOfDay" value="dinner" />
         <label htmlFor="dinner">Dinner</label><br /><br />
 
-        <label htmlFor="fridgeContents">What do you have in your fridge?</label><br />
-        <textarea id="fridgeContents" name="fridgeContents" rows="4" cols="50"></textarea><br /><br />
+        <label htmlFor="fridgeContents"><h3>What do you have in your fridge?</h3></label><br />
+        <textarea id="fridgeContents" name="fridgeContents" rows={4} cols={50} placeholder="separate items by comma"></textarea><br /><br />
 
-        <input type="submit" value="Submit" onClick="submitForm()" />
+        <input type="submit" value="Submit" onClick={submitForm} />
 
       </Form>
     </div>
@@ -77,11 +77,14 @@ function submitForm(): boolean {
   // Get form data
   const form = document.getElementById("mealPreferencesForm") as HTMLFormElement;
   const formData = new FormData(form);
+  const inputText = (document.getElementById("fridgeContents") as HTMLTextAreaElement)?.value || "";
+  const fridgeContents = inputText.split(',').map(item => item.trim());
+  console.log("Fridge Contents:", fridgeContents);
 
   // Convert FormData to object
-  const formObject: { [key: string]: string } = {};
+  const formObject: { [key: string]: string | string[] } = {};
   formData.forEach((value, key) => {
-    formObject[key] = value as string;
+    formObject[key] = key === "fridgeContents" ? fridgeContents : value as string;
   });
 
   // Log the data to the console
