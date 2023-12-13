85e51fe549dcaf9eaef8ca246a074bfae3eeda26
Kyleigh Poultney <kyleigh.poultney@gmail.com>
2023-12-13 12:59:43 -0500

    Updated account form inputs

--- src/routes/account.tsx
+++ src/routes/account.tsx
@@ -51,16 +51,39 @@ export const Account = () => {
             <h1>Tell Us More About <span className="accent">{userName}</span></h1>
             <p>Knowing more about you makes meal planning easier.</p>
             <Form method="post">
-              // family members
-              // any children
-              // dietary restrictions
-              // 
-              <input type="text"
-                name="userName"
+              <label htmlFor="email">What's your email?</label><br />
+              <input type="email"
                 required
-                placeholder="What is your name?"
-                aria-label="Your Name"
-                autoComplete="given-name" />
+                placeholder="name@name.com"
+                aria-label="Email" />
+              <br /><br />
+              <label htmlFor="familyMembers">How many people is this meal plan for?</label><br />
+              <input type="number"
+                required
+                placeholder="1"
+                min="1"
+                aria-label="How many family members?" />
+              <br /><br />
+              <label htmlFor="children">Are you cooking for children?</label><br />
+              <input type="checkbox" />
+              <label htmlFor="yes">Yes</label>
+              <input type="checkbox" />
+              <label htmlFor="no">No</label>
+              <br /><br />
+              <label htmlFor="dietaryType">Do you follow a specific diet?</label><br />
+              <input type="radio" name="dietaryType" id="vegetarian" value="vegetarian" />
+              <label htmlFor="vegetarian">Vegetarian</label><br />
+              <input type="radio" name="dietaryType" id="paleo" value="paleo" />
+              <label htmlFor="paleo">Paleo</label><br />
+              <input type="radio" name="dietaryType" id="pescatarian" value="pescatarian" />
+              <label htmlFor="pescatarian">Pescatarian</label><br />
+              <input type="radio" name="dietaryType" id="keto" value="keto" />
+              <label htmlFor="keto">Keto</label><br />
+              <input type="radio" name="lowFat" id="lowFat" />
+              <label htmlFor="lowFat">Low Fat</label>< br />
+              <input type="radio" name="none" id="none" />
+              <label htmlFor="none">None</label>
+              <br /><br />
               <input type="hidden" name="_action" value="accountUpdate" />
               <button type="submit">
                 <span>Update Account</span>
